48a14bd1bd0159d2de1f548d149e553164bcdde5
mvndaai <mvndaai+github@gmail.com>
2014-03-09 17:23:56 -0600

    Version 0.2.0
    Full rewrite, there are less options, but the failure logging is much better. It now includes a failure heatmap.

--- cucumber_atf.rb
+++ cucumber_atf.rb
@@ -2,302 +2,308 @@
 # Cucumber Automation Test Framework
 # Created by Jason Mavandi
 # mvndaai@gmail.com
-# Created Feb 7, 2013
-# Verion 0.1.0 
-#
-# Currently only works on Mac (maybe Linux)
-# * Will not work on Windows
-#
-# Current Features
-# - Choose a bus-web-auto-test from your computer
-# - Find all tags with @TC. excluding any tag you want
-# - List amount of tests to be run
-# - Run test cases from list of tags above
-# - Verify if test cases passed or failed
-# - Creates log file based on bus/phx versions 
-# - Create report of all passed/failed cases
-#
-#
-# Wanted Features
-# - Find all test cases missing important tags
-# - Work on windows
-#
+# Updated Mar 6, 2013
+# Verion 0.2.0 
 # -------------------------------------------------------------
 
-# Required Variables
-@cucumber_folder_name            #Takes a string
-@important_tags                  #Takes an array
-@log_folder_name = 'Cucumber_ATF'
-# Optional variables
-@unimportant_tags = ['#']        #Takes an array 
-@run_option                      #Takes a character, listed below 
-                  #(n)ormal, (c)lean, (r)etry non passed, (s)kip failed, run (f)ailed
+# -------------------------------------------------------------
+# Notes
+# - I use tag prefixes so I can run individual scenarios that have numbered tags
+# ex. @test.01, @test.02 will both be caught by @test
+# -------------------------------------------------------------
+
+## Fill in to skip questions
+#@project_directory = '/home/mavanj/bus-web-auto-test/'
+@project_directory = '/Users/mavanj/StashNext/bus_automation/automation/bus-web-auto-test/'
+@log_folder_name = 'log'
+@tag_prefixes_to_run = ['@TC.']
+@tags_to_skip = ['qa5','@phoenix','@aria']
+@tags = ['@TC.10491']
 
 END {
-verify_required_inputs
-@directory =  ask_which_directory(get_cucumber_directories)
-run_test_cases(@directory,get_tags)
-print "\n\n"
-} 
+  setup
+  collect_tags_from_directory
+  run_tags
+}
 
-#Setup
-def get_cucumber_directories
-  instances = []
-  #NOTE: change maxdepth if you want to check your computer deeper
-  folders = `cd ~/; find . 2>/dev/null -maxdepth 6 -type d | grep #{@cucumber_folder_name}`
-  folders.each_line do |line|
-    instances << line.chomp if line.include? "#{@cucumber_folder_name}\n" unless line.include? 'Trash'
-  end
-  instances 
-end
-def ask_which_directory(directory_array)
-  #This is how to choose with cucumber project if there are multipule on your computer
-  # If this file is within the cucumber project it will choose this
-  # If it cannot find a project on your computer it will tell you to download
-  # If there is only one installation on the computer it will choose that
-  # If there are multiple installations it will list them for you
-  return `pwd`.slice(0..(str.index(@cucumber_folder_name)))if `pwd`.include? @cucumber_folder_name
+# Setup 
+def setup
+  @os_windows = (/cygwin|mswin|mingw|bccwin|wince|emx/ =~ RUBY_PLATFORM) != nil
+  @os_mac = (/darwin/ =~ RUBY_PLATFORM) != nil
+  @os_linux = !@os_windows && !@os_mac
   
-  if directory_array.length == 0
-puts 'Please download the project'
-    abort
-  elsif directory_array.length == 1
-    return directory_array[0]
-  else
-    puts "\nChoose cucumber installation(#{@cucumber_folder_name})"
-    return ask_from_array(directory_array)
-  end
-end
-def verify_required_inputs
-  required_vars = [@cucumber_folder_name,@log_folder_name,@important_tags]
-  required_vars.each {|item| puts "Please fill in required items" if item.nil?}
-end
-
-def get_tags(run_option=nil)
-  @run_option ||= 'n'
-  run_option = @run_option if run_option.nil?
-
-  case(run_option)
-  when 'n' #(n)ormal
-    normal = get_non_passed_tags
-    return normal if normal.any?
-    get_all_tags(@directory)
-  when 'c' #(c)lean 
-    get_all_tags(@directory)
-  when 'r' #(r)etry non passed 
-    get_non_passed_tags
-  when 's' #(s)kip failed
-    get_all_tags(@directory) - get_failed_tags
-  when 'f' #run (f)ailed
-    get_failed_tags
-  end
-end
-def get_all_tags(directory)
-  files = `cd #{directory}; find . 2>/dev/null -maxdepth 7| grep -F "\.feature"`
-  tag_lines = ''
-  files.each_line do |file|
-    tag_lines += `grep '@TC' #{directory}#{file[1..-1]}`
-  end
-  tag_array = [] 
-  tag_lines.each_line do |line|
-    unless includes_any(line,@unimportant_tags)
-      line.split(' ').each{|item| tag_array << item if includes_any(item,@important_tags)}
-    end
-  end
-  tag_array
-end
-def get_failed_tags
-  cleanup_summary
-  if File.exists? File.join(log_folder_path,log_filename('summary'))
-    failed,passed = [],[]
-    summary = read_file(log_folder_path,log_filename('summary'))
-    summary.each_line do |line|
-      if line.include? 'Failed:'
-        line = line.strip.gsub(' ','').gsub('"','').gsub('Failed:','').gsub('[','').gsub(']','').split(',')
-        line.each do |tag|
-          failed << tag
-        end
-      end
-      if line.include? 'Passed:'
-        line = line.strip.gsub(' ','').gsub('"','').gsub('Passed:','').gsub('[','').gsub(']','').split(',')
-        line.each do |tag|
-          passed << tag
-        end
-      end
-    end
-  end
-  failed = failed.uniq - passed.uniq
-  failed
-end
-def get_non_passed_tags
-  cleanup_summary
-  if File.exists? File.join(log_folder_path,log_filename('summary'))
-    all,passed = [],[]
-    summary = read_file(log_folder_path,log_filename('summary'))
-    summary.each_line do |line|
-      if line.include? 'All:'
-        line = line.strip.gsub(' ','').gsub('"','').gsub('[','').gsub(']','').split(',').gsub('All:','')
-        line.each do |tag|
-          all << tag
-        end
-      end
-      if line.include? 'Passed:'
-        line = line.strip.gsub(' ','').gsub('"','').gsub('[','').gsub(']','').split(',').gsub('Passed:','')
-        line.each do |tag|
-          passed << tag
-        end
-      end
-    end
-  end
-  all = all.uniq - passed.uniq
-  all
-end
-
-#Running
-def run_test_cases(directory,array)
-  puts "Test cases to run: #{array.length}"
-  log_start(array)
+  #Ask questions if necessary
+  ask_project_directory if @project_directory.nil?
+  #verify_project_directory
+  ask_log_folder_name if @log_folder_name.nil?
+  ask_tag_questions if @tag_prefixes_to_run.nil?
   
-  passed,failed = [],[]
-  array.each.with_index do |tag,index|
-    cmd  = "cd #{directory}; bundle exec cucumber -t #{tag}"
-    puts "Running test case #{tag} (#{index+1} of #{array.length})"
-    report = `#{cmd}`
-    
-    result = cucumber_passed?(report)
-    puts "Test case #{result ? 'passed' : 'failed'}\n"
-    log_result(tag,result)
-    
-    if result
-      passed << tag 
+  #Create log folder/files
+  create_log_folder
+  name_log_files
+end
+def ask_project_directory
+  print "What is the directory of your cucumber project: "
+  @project_directory = question_not_blank
+  #puts @project_directory
+end
+def verify_project_directory
+  loop do
+    dir = @project_directory
+    @project_directory = dir.gsub('~/',"/Users/#{`whoami`.chomp}/") if dir.start_with?('~/')  if @os_mac
+    @project_directory = dir.gsub('~/',"/home/#{`whoami`.chomp}/") if dir.start_with?('~/')  if @os_linux 
+    break if File.directory?(@project_directory)
+    puts "The cucumber directory listed below does not exists, please try again"
+    puts "  '#{@project_directory}'"
+    ask_project_directory
+  end
+end
+def ask_log_folder_name
+  print "Name a log folder(default 'log'): "
+  @log_folder_name = STDIN.gets.chomp.strip
+  @log_folder_name = 'log' if @log_folder_name == ''
+  #puts @log_folder_name
+end
+def ask_tag_questions
+  print "List important starts of tags, separated by commas (Starting with @): "
+  @tag_prefixes_to_run = question_separated_by_commas
+  print "#{@tag_prefixes_to_run}\n"
+  if @tags_to_skip.nil?
+    print "Are there any tags you would like to skip?(y/n): "
+    if question_yes_no
+      print 'List tags to skip separated by commas: '
+      @tags_to_skip = question_separated_by_commas
+      print "#{@tags_to_skip}\n"
+    end
+  end
+end
+def create_log_folder
+  @log_directory = File.join(@project_directory,@log_folder_name)
+  return if File.directory?(@log_directory)
+  Dir::mkdir(@log_directory)
+  #puts @log_directory
+end
+def name_log_files
+  summary = "atf-summary.txt"
+  failure = "atf-fail-#{time}.txt"
+  @summary_log_name = File.join(@log_directory,summary)
+  @failure_log_name = File.join(@log_directory,failure)
+  #File.open(@summary_log, 'w') {|f| f.write("Start at #{Time.now}") }
+end
+
+#Gather tags from feature files
+def collect_tags_from_directory
+  return if @tags.any? if !!@tags #Hack to let you force tags instead of searching
+  @tags = []
+  features = find_feature_files(File.join(@project_directory,'features',''))
+  features.each do |feature|
+    @tags << scan_features_for_tags(feature)
+    @tags.flatten!.uniq!
+  end
+  #puts "#{@tags}\n"
+  #puts @tags.length
+end
+def find_feature_files(folder)
+  feature_files = []
+  Dir.glob("#{File.join(folder,'*','')}").each do |sub_folder|
+    feature_files << Dir.glob("#{sub_folder}*.feature")
+    find_feature_files(sub_folder)
+  end
+  feature_files.flatten!
+end
+def scan_features_for_tags(feature)
+  tag_lines = get_feature_tag_lines(feature) 
+  get_tags_from_lines(tag_lines)
+end
+def get_feature_tag_lines(feature)
+  tag_lines = []
+  #Grab all Tag Lines
+  File.open(feature, 'r') do |f| 
+    f.each_line do |line|
+      line.strip!
+      next if line.start_with?('#')
+      if line.start_with?('@')
+        if !!@tags_to_skip ##Skipping unwanted tags
+          move_on = false
+          @tags_to_skip.each do |bad_tag|
+            if line.include? bad_tag
+              move_on = true
+              break
+            end
+          end
+          next if move_on
+        end
+      end
+      tag_lines << line 
+    end
+  end
+  tag_lines
+end
+def get_tags_from_lines(tag_lines)
+  important = []
+  #puts "\n\n#{tag_lines}"
+  tag_lines.each do |line|
+    #puts line
+    line.split(' ').each do |tag|
+      @tag_prefixes_to_run.each do |prefix|
+        #puts "#{prefix} - #{tag}"
+        important << tag if tag.include? prefix  
+      end
+    end
+  end
+  #print "#{important}\n"
+  important
+end
+
+#Running and logging
+def run_tags
+  total = @tags.length
+  puts "Running #{total} test case#{total == 1 ? '' : 's'}"
+  pass,fail = [],[]
+  @tags.each.with_index do |tag,index|
+    puts "Executing #{tag} (#{index+1} of #{total})"
+    log = launch_cucumber(tag)
+    result = cucumber_passed?(log)
+    if result #True if passed
+      pass << tag
     else
-      failed << tag
-      log_failure(report)
+      fail << tag
+      log_failure(tag,log)      
     end
+    string = "-- #{result ? "\e[32mPassed\e[0m" : "\e[31mFailed\e[0m"} at #{time}"
+    string += " (#{pass.length} passed, #{fail.length} failed)\n" 
+    puts string
+    update_summary(tag) 
   end
-  print "Passed: #{passed}\n"
-  print "Failed: #{failed}\n"
+  puts "Done! Totals:#{pass.length} passed, #{fail.length} failed.\n\n"
+  puts @fail_heatmap
 end
+def launch_cucumber(tag)
+  `cd #{@project_directory}; bundle exec cucumber -t #{tag}` 
+end  
 def cucumber_passed?(string)
   (string.include? 'Failing Scenarios') ? false : true
 end
-
-## Log stuff
-def log_folder_path
-  `cd #{@directory}; mkdir log` unless `cd #{@directory}; ls`.include? 'log'
-  log_folder = File.join(@directory,'log')
-  `cd #{log_folder}; mkdir #{@log_folder_name}` unless `cd #{log_folder}; ls`.include? @log_folder_name  
-  File.join(@directory,'log',@log_folder_name)
+def log_failure(tag,log)
+# @failure_log
+  parsed = parse_failure(tag,log)
+  save = update_failure_heatmap(parsed)
+  save += add_fail_description(parsed)
+  
+  #Catch multipule scenario failures
+  #-Split log by @ begining of stripped line
+  
+  File.open(@failure_log_name, 'w') {|f| f.write(save) } 
 end
-def log_filename(string)
-  if instance_variable_get("@#{string}").nil?
-    instance_variable_set("@#{string}", "#{string}.log") 
-  end
-  instance_variable_get("@#{string}")
-end
-def log_start(tags)
-  string = "Recent (#{time})\nAll:#{tags}\n"
-  if @run_option == 'c'
-    if File.exists? File.join(log_folder_path,log_filename('summary'))
-      old_log = read_file(log_folder_path,log_filename('summary'))
-      filename = "#{log_filename('summary').gsub('.log','')}_before_#{time}.log"
-      create_file(old_log,log_folder_path,filename)
-      create_file(string,log_folder_path,log_filename('summary'))
-      return
+def parse_failure(tag,log)
+  #Note bash printouts have color they start with a [#m and end with [0m
+  # [31m is red, [90m is gray
+  
+  failure_array = []
+  log = log.split(tag) #Catches multipule scenarios with one tag
+  log.each.with_index do |scenario,index|
+    next if index == 0 #Skip First
+    red_lines = '' 
+    scenario.each_line do |line|
+      line = line.lstrip.gsub('\e','')
+      red_lines += line if line[0,6].include?("[31m") #Starts with red
     end
+    next if red_lines == ''
+    
+    red_lines = remove_bash_colors(red_lines).split('Failing Scenarios:')
+    failure = Hash.new
+    failure['scenario'] = red_lines[1].split('common ')[1]
+    failure['step'] = red_lines[0].split('\n')[-1]
+    failure['tag'] = tag    
+        
+    failure_array << failure
   end
-  append_to_file(string,log_folder_path,log_filename('summary'))    
+  #puts failure_array
+  failure_array
+end  
+def remove_bash_colors(string)
+  remove_characters(string,['[0m','[1m','[90m','[36m','[32m','[31m',"\e"])
 end
-def log_failure(report)
-  weird = ['[0m','[1m','[31m','[32m','[36m','[90m']
-  weird.each {|string| report = report.gsub(string,'')}
-  string = "\n\n#{report}"
-  append_to_file(string,log_folder_path,log_filename('failures'))    
+def remove_characters(string,character_array)
+  character_array.each {|char| string = string.gsub(char,'')}
+  string
 end
-def log_result(tag,result)
-  string = result ? "#{tag}-Passed\n" : "#{tag}-Failed\n" 
-  append_to_file(string,log_folder_path,log_filename('summary'))    
-end
-def cleanup_summary
-  if File.exists? File.join(log_folder_path,log_filename('summary'))
-    summary = read_file(log_folder_path,log_filename('summary'))
-    if summary.include? 'Recent'
-      summary = summary.gsub('Previous','Past')
-      passed,failed,progress,recent = [],[],'',false
-      summary.each_line do |line|
-        progress += line if line.include? 'All:'
-        passed << line.split('-')[0] if line.include? '-Passed' if recent
-        failed << line.split('-')[0] if line.include? '-Failed' if recent
-        if line.include? 'Recent'
-          progress += line.gsub('Recent','Previous')
-          recent = true
-        end        
-        progress += line unless recent
-      end
-      progress += "Passed:#{passed}\n"
-      progress += "Failed:#{failed}\n\n"
-      create_file(progress,log_folder_path,log_filename('summary'))
+def update_failure_heatmap(array_of_hashes)
+  @failure_heatmap_hash ||= Hash.new
+  array_of_hashes.each do |hash|
+    step = hash['step'].split("\n")[-1].split('in `')[1].strip
+    #TODO figure out how to remove the ' at the end
+    if @failure_heatmap_hash[step].nil?
+      @failure_heatmap_hash[step] = 1
+    else
+      @failure_heatmap_hash[step] = @failure_heatmap_hash[step] + 1 
     end
-  end
-end
-
-#Interact with Files
-def append_to_file(string,filepath,filename)
-  File.open(File.join(filepath,filename), 'a') {|f| f.write(string) }
-end
-def create_file(string,filepath,filename)
-  File.open(File.join(filepath,filename), 'w') {|f| f.write(string) }
-end
-def read_file(filepath,filename)
-  path = File.join(filepath,filename)
-  `cat #{path}`
-end
-
-##### Asking Questions Helpers
-def input_within(input,low,high)
-  #puts "Input: #{input}\nLow: #{low}\nHigh: #{high}"
-  return true if input >= low && input <= high
-  false
-end
-def is_numeric?(obj) 
-   obj.to_s.match(/\A[+-]?\d+?(\.\d+)?\Z/) == nil ? false : true
-end
-def ask_from_array(array)
-  array.each.with_index do |item,index|
-    puts "#{index+1}) #{item}"
   end
   
-  while true
-    selection = gets.chomp
-    if is_numeric?(selection)
-      return array[selection.to_i-1] if input_within(selection.to_i,1,array.length)
-    elsif
-      possibliities = Array.new
-      array.each.with_index do |item,index|
-        possibliities << index if item.downcase.include? selection.downcase.strip
-      end
-      return array[possibliities[0]] if possibliities.length == 1
-    end
-    puts "#{selection} is not a valid selection, try again:"
+  #Get the longest string for making the print out pretty
+  longest_string = 0
+  @failure_heatmap_hash.each do |k,_|
+    longest_string = k.length if k.length > longest_string
   end
+  
+  line = "#{'-'*(longest_string + 12)}\n"
+  @fail_heatmap = "Failure Heat Map:\n#{line}" 
+  @fail_heatmap += "| #{'Count'.ljust(5)} | #{'Step'.ljust(longest_string)} |\n"
+  @failure_heatmap_hash.sort_by {|k,v| v}.reverse.each do |k,v|
+    @fail_heatmap += "| #{v.to_s.rjust(5)} | #{k.to_s.ljust(longest_string)} |\n"  
+  end
+  @fail_heatmap += line
+  #puts string
+  @fail_heatmap
+end
+def add_fail_description(array_of_hashes)
+  @fail_tail ||= "\n\nFailure Logs:\n"
+  array_of_hashes.each do |hash|
+    @fail_tail += "Code ran: bundle exec cucumber -t #{hash['tag']}\n"
+    @fail_tail += "Failure location: #{hash['scenario']}"
+    @fail_tail += "#{hash['step']}\n\n"
+  end
+  @fail_tail
 end
 
-#Other Helpers
-def includes_any(string,include_array,exclude_array=nil)
-  #TODO make faster by checking if exclude array exists once instead of each time
-  include_array.each do |item|
-    if string.include? item
-      exclude_array.each { |excluded|  return false if string.include? excluded} if !!exclude_array
-      return true
-    end
-  end 
-  false
+def update_summary(tag)
+end
+
+#Generic Helpers
+def time
+  Time.now.to_s[0...-6].gsub(' ','_')
 end
 def combine_arrays(array_a,array_b)
   array_a << array_b
   array_a.flatten!
   array_a.uniq!
 end
-def time
-  Time.now.to_s[0...-6].gsub(' ','_')
+def question_yes_no
+  loop do
+    responce = STDIN.gets.chomp.strip.downcase
+    boolean = responce == 'y' || responce == 'n' || responce == 'yes' || responce == 'no'
+    if boolean
+      return true if responce[0,1] == 'y'
+      return false
+    end
+    print "Please respond with (y)es or (n)o: "
+  end
+end
+def question_not_blank
+  loop do
+    responce =  STDIN.gets.chomp.strip
+    return responce if responce != ''
+    print 'Please type something: '
+  end
+end
+def question_separated_by_commas
+  split_and_strip(question_not_blank,',')
+end
+def split_and_strip(string,deliminer)
+  complete = []
+  string.split(deliminer).each {|item| complete << item.strip}
+  complete
+end
+def test_file(string)
+   File.open(File.join(@log_directory,'test.txt'), 'w') {|f| f.write(string) } 
 end
